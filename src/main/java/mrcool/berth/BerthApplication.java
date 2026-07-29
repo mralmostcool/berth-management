5b29b7474eb0d211bbd7970ce52757b1bf6c1aa1
Neeraj Gupta <theneerajgupta@gmail.com>
2026-07-29 13:22:01 +0530

    feat: init base project setup with Spring Boot, Flyway, Scalar, and dotenv

---
+++ src/main/java/mrcool/berth/BerthApplication.java
@@ -0,0 +1,13 @@
+package mrcool.berth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BerthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BerthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mrcool/berth/BerthApplicationTests.java
@@ -0,0 +1,13 @@
+package mrcool.berth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BerthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

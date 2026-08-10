b8720d4c8c280cee4cbf5d38bdd797a1d431b89c
Neeraj Gupta <theneerajgupta@gmail.com>
2026-08-10 12:20:08 +0530

    refactor: clean up FlagsService to a simple boilerplate placeholder
    - Remove temporary/unused findById and getFlagNameById methods from FlagsService to keep all services standardized as boilerplate placeholders.

--- src/main/java/mrcool/berth/service/FlagsService.java
+++ src/main/java/mrcool/berth/service/FlagsService.java
@@ -1,11 +1,8 @@
 package mrcool.berth.service;
 
-import java.util.UUID;
-
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import mrcool.berth.model.Flags;
 import mrcool.berth.repository.FlagsRepository;
 
 @Service
@@ -13,14 +10,5 @@ import mrcool.berth.repository.FlagsRepository;
 public class FlagsService {
 
     private final FlagsRepository flagsRepository;
-
-    public String getFlagNameById(UUID id) {
-        String name = flagsRepository.findNameById(id);
-        return name;
-    }
-
-    public Flags findById(UUID id) throws Exception {
-        return flagsRepository.findById(id).orElseThrow(() -> new Exception("Entry not found!"));
-    }
 
 }
